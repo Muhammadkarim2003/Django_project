84b4567d12658832aa9743d6998c2fd96c419fe2
Sukhoi2003 <o6022648@gmail.com>
2023-10-02 20:23:28 +0500

    template qushildi

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+
+data = [
+    {
+        'id' : '1',
+        'title': 'Ekonom vewsayt',
+        'description': 'Ekonimoka'
+    },
+    {
+        'id' : '2',
+        'title': 'Oyin sayt',
+        'description': 'Wargame'
+    },
+    {
+        'id' : '3',
+        'title': 'Portfoliya vewsayti',
+        'description': 'Portfoliyalar'
+    }
+]
+
 
 # Create your views here.
+def projects(request):
+    return render(request, 'projects.html', {'data':data})
+
+def project(request, pk):
+    content = None
+    for project in data:
+        if project['id'] == str(pk):
+            content = project
+    return render(request, 'project.html', context=content)
